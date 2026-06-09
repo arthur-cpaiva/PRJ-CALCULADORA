205e9a14095eb9b7ce11b392d075df28d79e0a98
Arthur Paiva <arthur-68@hotmail.com>
2026-06-08 21:09:46 -0300

    Adicionando funções

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,46 @@
+//declaração de variáveis constantes
+const botao = document.querySelector("#btnCalcular");
+const numero1 = document.querySelector("#num1");
+const numero2 = document.querySelector("#num2");
+const resultado = document.querySelector("#resultado");
+
+
+//área das funções
 function somar() {
 
-    let numero1, numero2, resultado;
+    let num1, num2, resCalculo;
 
-    numero1 = Number(document.querySelector('#num1').value);
-    numero2 = Number(document.querySelector('#num2').value);
-    resultado = document.querySelector("#resultado").innerHTML;
+    num1 = Number(numero1.value);
+    num2 = Number(numero2.value);
 
-    resultado = numero1 + numero2;
-
-    document.querySelector("#resultado").innerHTML = resultado;
+    console.log(isNaN(num1));
+    console.log(isNaN(num2));
+    
+    resCalculo = num1 + num2;
+    resultado.innerHTML = resCalculo;
 }
+
+function validarEntradas() {
+    if (isNaN(numero1.value) || isNaN(numero2.value)) {
+        resultado.innerHTML = "Valores inválidos. Insira novos valores (números)"
+        return false
+    } else {
+        return true
+    }
+}
+
+function mostrarMensagemdeErro() {
+    resultado.innerHTML = "Valores inválidos. Insira novos valores (números)"
+}
+
+//O programa começa aqui
+
+if (validarEntradas() == true) {
+    botao.addEventListener("click", somar)
+} else {
+    mostrarMensagemdeErro();
+}
+
+
+
+botao.addEventListener("click", somar);
