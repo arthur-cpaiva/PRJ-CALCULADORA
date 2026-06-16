819adca610fa74ae2c3f6ec245a2da0b9c89deb0
Arthur Paiva <arthur-68@hotmail.com>
2026-06-15 22:24:16 -0300

    Functions novas

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,18 @@
 //declaração de variáveis constantes
-const botao = document.querySelector("#btnCalcular");
+const botao = document.querySelector("#btnSomar");
 const numero1 = document.querySelector("#num1");
 const numero2 = document.querySelector("#num2");
 const resultado = document.querySelector("#resultado");
+const botaoSubtracao = document.querySelector("#btnSubtrair")
+const botaoMultiplicacao = document.querySelector("#btnMultiplicar")
+const botaoDivisao = document.querySelector("#btnDividir")
+const botaoLimpar = document.querySelector("#btnLimpar")
+const caixaResultado = document.querySelectorAll(".box")[1];
+
 
 
 //área das funções
-function somar() {
+function somar() { //sem parâmetro e sem retorno
 
     let num1, num2, resCalculo;
 
@@ -14,12 +20,81 @@ function somar() {
         num1 = Number(numero1.value);
         num2 = Number(numero2.value);
         resCalculo = num1 + num2;
+        caixaResultado.classList.remove("invisible");
+        limparCaixa(numero1.value & numero2.value);
         exibirResultado(resCalculo); //com parâmetro e sem retorno
     } else {
         mostrarMensagemErro(); //sem parâmetro e sem retorno
     }
 
 }
+
+function subtrair() {
+    let num1, num2, resCalculo;
+
+    if (validarEntradas() === true) { // sem parâmetro e com retorno /*sempre que criar uma função, adicionar o parenteses, mesmo que não tenha nada*/
+        num1 = Number(numero1.value);
+        num2 = Number(numero2.value);
+        resCalculo = num1 - num2;
+        caixaResultado.classList.remove("invisible");
+        limparCaixa(numero1.value & numero2.value);
+        exibirResultado(resCalculo); //com parâmetro e sem retorno
+    } else {
+        mostrarMensagemErro(); //sem parâmetro e sem retorno
+    }
+
+
+}
+
+function multiplicar() {
+    let num1, num2, resCalculo;
+
+    if (validarEntradas() === true) { // sem parâmetro e com retorno /*sempre que criar uma função, adicionar o parenteses, mesmo que não tenha nada*/
+        num1 = Number(numero1.value);
+        num2 = Number(numero2.value);
+        resCalculo = num1 * num2;
+        caixaResultado.classList.remove("invisible");
+        limparCaixa(numero1.value & numero2.value);
+        exibirResultado(resCalculo); //com parâmetro e sem retorno
+    } else {
+        mostrarMensagemErro(); //sem parâmetro e sem retorno
+    }
+}
+
+function dividir() {
+    let num1, num2, resCalculo;
+
+    if (validarEntradas() === true) { // sem parâmetro e com retorno /*sempre que criar uma função, adicionar o parenteses, mesmo que não tenha nada*/
+        num1 = Number(numero1.value);
+        num2 = Number(numero2.value);
+        if (num2 = Number(numero2.value) === 0) {
+            caixaResultado.classList.remove("invisible");
+            erroDivisao();
+        }
+
+        else {
+            resCalculo = num1 / num2;
+            caixaResultado.classList.remove("invisible");
+            limparCaixa(numero1.value & numero2.value);
+            exibirResultado(resCalculo); //com parâmetro e sem retorno
+        }
+    } else {
+        mostrarMensagemErro(); //sem parâmetro e sem retorno
+    }
+}
+
+function limparCampos() {
+    numero1.value = "";
+    numero2.value = "";
+    resultado.innerHTML = "";
+    caixaResultado.classList.add("invisible");
+}
+
+function limparCaixa() {
+    numero1.value = "";
+    numero2.value = "";
+}
+
 
 function validarEntradas() { //sem parâmetro e com retorno
     if (isNaN(numero1.value) || isNaN(numero2.value)) { //com parâmetro e com retorno
@@ -29,15 +104,22 @@ function validarEntradas() { //sem parâmetro e com retorno
     }
 }
 
-//O programa começa aqui
 
 function mostrarMensagemErro() { //sem parâmetro e com retorno
     resultado.innerHTML = "Valores inválidos. Insira novos valores (números)";
 }
 
-function exibirResultado(valor) {  //com parâmetro e sem retorno /*sempre que criar uma função, adicionar o parenteses, mesmo que não tenha nada*/
-    resultado.innerHTML = valor;
+function erroDivisao() {
+    resultado.innerHTML = "Valores inválidos - Divisão por zero";
 }
 
+function exibirResultado(valor) {  //com parâmetro e sem retorno /*sempre que criar uma função, adicionar o parenteses, mesmo que não tenha nada*/
+    resultado.innerHTML = "Resultado: " + valor;
+}
 
+//O programa começa aqui
 botao.addEventListener("click", somar); //com parâemtro e sem retorno
+botaoSubtracao.addEventListener("click", subtrair);
+botaoMultiplicacao.addEventListener("click", multiplicar);
+botaoDivisao.addEventListener("click", dividir);
+botaoLimpar.addEventListener("click", limparCampos);
